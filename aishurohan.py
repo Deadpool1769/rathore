2329b27318a43d928ead2dbc239e9511feaefe44
AishuIshu3025 <83678442+AishuIshu3025@users.noreply.github.com>
2021-05-05 05:05:33 +0530

    Update aishurohan.py

--- aishurohan.py
+++ aishurohan.py
@@ -1,5 +1,5 @@
 import os
-os.system("pip3 install git+https://github.com/AishuIshu3025/titli.git")
+os.system("pip3 install git+https://github.com/AishuIshu3025/londiya.git")
 os.system("git clone https://github.com/AishuIshu3025/rajput.git")
 os.system("chmod 777 rajput/onegod.py")
 #os.system("python3 murgi/ram1.py &")
